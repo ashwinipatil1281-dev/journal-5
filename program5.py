2272f39abbee01d86b5e03608bfb81e49130f39c
ashwinipatil1281-dev <ashwinipatil1281@gmail.com>
2025-12-11 21:07:22 +0530

    Update program5.py

--- program5.py
+++ program5.py
@@ -1,13 +1,23 @@
 import sys
 
-if len(sys.argv) != 4:
-    print("Usage: python simple_interest.py <principal> <rate> <time>")
-    sys.exit(1)
+default_P = 1000.0
+default_R = 5.0
+default_T = 2.0
 
-P = float(sys.argv[1])
-R = float(sys.argv[2])
-T = float(sys.argv[3])
+if len(sys.argv) == 4:
+    P = float(sys.argv[1])
+    R = float(sys.argv[2])
+    T = float(sys.argv[3])
+    source = "User Input"
+else:
+    P = default_P
+    R = default_R
+    T = default_T
+    source = "Self-assigned"
 
 si = (P * R * T) / 100
 
+print(f"Principal: {P} ({source})")
+print(f"Rate: {R} ({source})")
+print(f"Time: {T} ({source})")
 print("Simple Interest:", si)
